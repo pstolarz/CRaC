2090f8d1b8ed272599adc932411e1a91b8922067
Piotr Stolarz <pstolarz@o2.pl>
2026-09-11 09:24:59 +0200

    using detail removed from crac namespace; _names fixed

--- include/crac_test.h
+++ include/crac_test.h
@@ -16,37 +16,37 @@
 namespace {
 
 // std::is_same counterpart
-template<typename T1, typename T2> struct _is_same { constexpr static bool value = false; };
-template<typename T> struct _is_same<T, T> { constexpr static bool value = true; };
-template<typename T1, typename T2> constexpr static bool _is_same_v = _is_same<T1, T2>::value;
+template<typename T1, typename T2> struct is_same { constexpr static bool value = false; };
+template<typename T> struct is_same<T, T> { constexpr static bool value = true; };
+template<typename T1, typename T2> constexpr static bool is_same_v = is_same<T1, T2>::value;
 
 //
 // CRC output types test
 //
-static_assert(_is_same_v<CRC1::type, uint8_t>);
-static_assert(_is_same_v<CRC3_GSM::type, uint8_t>);
-static_assert(_is_same_v<CRC4_ITU::type, uint8_t>);
-static_assert(_is_same_v<CRC5_USB::type, uint8_t>);
-static_assert(_is_same_v<CRC6_ITU::type, uint8_t>);
-static_assert(_is_same_v<CRC7::type, uint8_t>);
-static_assert(_is_same_v<CRC8::type, uint8_t>);
-static_assert(_is_same_v<CRC10_GSM::type, uint16_t>);
-static_assert(_is_same_v<CRC11_UMTS::type, uint16_t>);
-static_assert(_is_same_v<CRC12_DECT::type, uint16_t>);
-static_assert(_is_same_v<CRC13_BBC::type, uint16_t>);
-static_assert(_is_same_v<CRC14_GSM::type, uint16_t>);
-static_assert(_is_same_v<CRC15::type, uint16_t>);
-static_assert(_is_same_v<CRC16_DECT_R::type, uint16_t>);
-static_assert(_is_same_v<CRC17_CAN_FD::type, uint32_t>);
-static_assert(_is_same_v<CRC21_CAN_FD::type, uint32_t>);
-static_assert(_is_same_v<CRC24_BLE::type, uint32_t>);
-static_assert(_is_same_v<CRC30_CDMA::type, uint32_t>);
-static_assert(_is_same_v<CRC31_PHILIPS::type, uint32_t>);
-static_assert(_is_same_v<CRC32_XFER::type, uint32_t>);
-static_assert(_is_same_v<CRC40_GSM::type, uint64_t>);
-static_assert(_is_same_v<CRC64_GO_ISO::type, uint64_t>);
+static_assert(is_same_v<CRC1::type, uint8_t>);
+static_assert(is_same_v<CRC3_GSM::type, uint8_t>);
+static_assert(is_same_v<CRC4_ITU::type, uint8_t>);
+static_assert(is_same_v<CRC5_USB::type, uint8_t>);
+static_assert(is_same_v<CRC6_ITU::type, uint8_t>);
+static_assert(is_same_v<CRC7::type, uint8_t>);
+static_assert(is_same_v<CRC8::type, uint8_t>);
+static_assert(is_same_v<CRC10_GSM::type, uint16_t>);
+static_assert(is_same_v<CRC11_UMTS::type, uint16_t>);
+static_assert(is_same_v<CRC12_DECT::type, uint16_t>);
+static_assert(is_same_v<CRC13_BBC::type, uint16_t>);
+static_assert(is_same_v<CRC14_GSM::type, uint16_t>);
+static_assert(is_same_v<CRC15::type, uint16_t>);
+static_assert(is_same_v<CRC16_DECT_R::type, uint16_t>);
+static_assert(is_same_v<CRC17_CAN_FD::type, uint32_t>);
+static_assert(is_same_v<CRC21_CAN_FD::type, uint32_t>);
+static_assert(is_same_v<CRC24_BLE::type, uint32_t>);
+static_assert(is_same_v<CRC30_CDMA::type, uint32_t>);
+static_assert(is_same_v<CRC31_PHILIPS::type, uint32_t>);
+static_assert(is_same_v<CRC32_XFER::type, uint32_t>);
+static_assert(is_same_v<CRC40_GSM::type, uint64_t>);
+static_assert(is_same_v<CRC64_GO_ISO::type, uint64_t>);
 #ifdef _CRAC_USE_EXTINT
-static_assert(_is_same_v<CRC82_DARC::type, __uint128_t>);
+static_assert(is_same_v<CRC82_DARC::type, __uint128_t>);
 #endif
 
 //
